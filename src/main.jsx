b3d90f383a4d1ce02041b010f8a6fe4e0bd4eb43
BEHEMOTH\Richard <yenraven@gmail.com>
2017-04-02 16:09:12 -0500

    smtn test

--- src/main.jsx
+++ src/main.jsx
@@ -404,12 +404,13 @@ class Main extends React.Component {
                 for(var z = 0; z < depth; z++){
                     var t = this.simplex.noise3D(x/16, y/16, z/16) * 0.5 + 0.5;
                     t += this.simplex.noise3D(x/32, y/32, z/32) * 0.5 + 0.5;
-                    var mtn = (1 - z/depth) * 0.5 + 0.5;
+                    var sheight = (1 - z/depth) * 0.5 + 0.5;
                     var dx = Math.abs(width/2 - x);
                     var dy = Math.abs(height/2 - y);
                     var dist = 1 - ((dx / (width/2)) * (dy / (height/2)));
                     var sdist = dist * 0.5 + 0.5;
-                    t = (t/2) * mtn * sdist > 0.5;
+                    var smtn = (sheight + sdist) / 2;
+                    t = (t/2) * smtn > 0.5;
 
 
                     // let a1, a2, a3, a4, a5, a6;
