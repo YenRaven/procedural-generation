95565dbfdcf9a9f934ecca9c45a4e7b5017b7a9e
BEHEMOTH\Richard <yenraven@gmail.com>
2017-04-03 16:01:31 -0500

    force client for debug.

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -88,6 +88,8 @@
 	
 	var CSS_COLOR_NAMES = (0, _shuffle2.default)(["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"]);
 	
+	var debugClient = true;
+	
 	var Main = function (_React$Component) {
 	    _inherits(Main, _React$Component);
 	
@@ -213,7 +215,7 @@
 	                        console.log('Synchronization succeeded');
 	                    }
 	                };
-	                if (nextState.user.isModerator) {
+	                if (nextState.user.isModerator && !debugClient) {
 	                    this.sync.instance.set(nextState.sync, onComplete);
 	                } else {
 	                    if (JSON.stringify(this.state.sync.climb[this.state.user.displayName]) != JSON.stringify(nextState.sync.climb[nextState.user.displayName])) {
@@ -412,7 +414,7 @@
 	                                    sync: syncVals
 	                                });
 	                            }
-	                        } else if (_this3.state.user.isModerator) {
+	                        } else if (_this3.state.user.isModerator && !debugClient) {
 	                            _this3.newWorld();
 	                        }
 	                    };
